0c3e5cdc419ac0eea1b696ce579d4521ba624ca3
Denizard <denizard.oliveira@gmail.com>
2024-09-04 15:53:23 -0300

    teste locais

--- escola/escola/settings.py
+++ escola/escola/settings.py
@@ -1,24 +1,98 @@
 import os
-import dj_database_url
-import django_heroku
+from pathlib import Path
 
-# Configuração do banco de dados
+# Caminho base do projeto
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Configurações gerais
+SECRET_KEY = 'sua-secret-key-aqui'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+# Aplicações instaladas
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'alunos',  # Substitua pelo nome da sua aplicação
+]
+
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Configuração da URL raiz
+ROOT_URLCONF = 'escola.urls'
+
+# Configurações de templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Configuração do WSGI
+WSGI_APPLICATION = 'escola.wsgi.application'
+
+# Configuração do banco de dados local
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'escola',
+        'USER': 'denizard',
+        'PASSWORD': 'denizdeniz',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
 
-# Verifica se estamos em ambiente local
-if 'DATABASE_URL' not in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'mysql.connector.django',
-            'NAME': 'escola',
-            'USER': 'denizard',
-            'PASSWORD': 'denizdeniz',
-            'HOST': 'localhost',
-            'PORT': '3306',
-        }
-    }
+# Configurações de senhas
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
-# Configuração do Heroku
-django_heroku.settings(locals())
+# Internacionalização
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Arquivos estáticos
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Configuração da chave primária padrão
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
